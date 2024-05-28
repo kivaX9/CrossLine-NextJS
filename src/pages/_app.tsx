69c47c1d56c1806ce558c71f010924623c58e90a
kivaX9 <kirill.vlasov.05@inbox.tu>
2024-05-28 23:44:32 +0500

    подключил material-ui и настроил prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter'
+import type { DocumentHeadTagsProps } from '@mui/material-nextjs/v13-pagesRouter'
+import { DocumentProps } from 'next/dist/pages/_document'
+import ScopedCssBaseline from '@mui/material/ScopedCssBaseline'
+
+export default function App(
+  { Component, pageProps }: AppProps,
+  props: DocumentProps & DocumentHeadTagsProps,
+) {
+  return (
+    <AppCacheProvider {...props}>
+      <ScopedCssBaseline>
+        <Component {...pageProps} />
+      </ScopedCssBaseline>
+    </AppCacheProvider>
+  )
 }
